13e82d90aee56549414ba81f728117f88ad07a4f
Jonathan Laluces <jlalucescareer123@gmail.com>
2022-01-04 13:01:28 -0500

    Hero and Button stories: added basic stories for both, framer-motion and storybook setup complete

---
+++ ui-gallery/src/components/Button.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+
+const Button = ({ label }) => {
+  return (
+    <motion.button
+      animate={{ x: 100 }}
+      transition={{ ease: 'easeOut', duration: 2 }}
+    >
+      {label}
+    </motion.button>
+  );
+};
+
+Button.propTypes = {
+  label: PropTypes.string,
+};
+
+export default Button;
---
+++ ui-gallery/src/components/Hero.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+
+const Hero = () => {
+  return (
+    <motion.h1
+      animate={{
+        opacity: 1,
+      }}
+      transition={{ duration: 1.4 }}
+      initial={{
+        opacity: 0.1,
+      }}
+    >
+      Framer Hero
+    </motion.h1>
+  );
+};
+
+export default Hero;
